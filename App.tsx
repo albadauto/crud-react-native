44a57ddec1b96fe0f773fb6956217426f6afff6e
albadev <joseadauto923@gmail.com>
2022-08-02 18:08:05 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,28 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import About from './src/screens/About/Index';
+import Home from './src/screens/Home/Index';
+import Ionicons from 'react-native-vector-icons/Ionicons'
 export default function App() {
+  const Tab = createBottomTabNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      <Tab.Navigator >
+        <Tab.Screen name="Home" component={Home} options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          )
+        }} />
+        <Tab.Screen name="Sobre" component={About} options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="alert" color={color} size={size} />
+          )
+        }} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
